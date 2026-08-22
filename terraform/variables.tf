b35c7e433050c4400b4f245376f6b147cb368e9b
Ramoon Camacho <ramooncamacho@gmail.com>
2026-08-22 14:30:21 -0300

    fix(database): update rds postgresql engine version to 16.3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,7 +39,7 @@ variable "aws_base_state_region" {
 variable "db_engine_version" {
   description = "PostgreSQL engine version"
   type        = string
-  default     = "16.3"
+  default     = "16.9-R2"
 }
 
 variable "db_instance_class" {
